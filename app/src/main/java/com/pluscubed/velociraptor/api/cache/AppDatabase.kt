2826971220071c54e2579c3c2fad081d61d8d139
plusCubed <pluscubed@users.noreply.github.com>
2019-01-04 17:12:59 -0800

    Bump 1.6.11

--- app/src/main/java/com/pluscubed/velociraptor/api/cache/AppDatabase.kt
+++ app/src/main/java/com/pluscubed/velociraptor/api/cache/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.pluscubed.velociraptor.api.cache
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Way::class], version = 8)
+@Database(entities = [Way::class], version = 9)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun wayDao(): WayDao
 }
