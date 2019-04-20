7098d1de327109e9f0d339ccc6c4fb96ef0d66b6
plusCubed <pluscubed@users.noreply.github.com>
2019-04-20 01:26:01 -0700

    Allow "only gmap navigation" without accessibility service

---
+++ app/src/main/java/com/pluscubed/velociraptor/detection/GmapsNavNotificationListener.kt
@@ -0,0 +1,56 @@
+package com.pluscubed.velociraptor.detection
+
+import android.app.Notification
+import android.content.Intent
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.text.TextUtils
+
+import com.pluscubed.velociraptor.limit.LimitService
+import com.pluscubed.velociraptor.utils.PrefUtils
+
+class GmapsNavNotificationListener : NotificationListenerService() {
+
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        if (sbn == null) {
+            return
+        }
+
+        val packageName = sbn.packageName
+        if (TextUtils.isEmpty(packageName)
+            || packageName != AppDetectionService.GOOGLE_MAPS_PACKAGE
+            || sbn.notification.priority != Notification.PRIORITY_MAX
+            || !PrefUtils.isGmapsOnlyInNavigation(this)
+        ) {
+            return
+        }
+
+        AppDetectionService.get()?.setGmapsNavigating(true)
+
+        val intent = Intent(this, LimitService::class.java)
+        intent.putExtra(LimitService.EXTRA_NOTIF_START, true)
+        startService(intent)
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification?) {
+        if (sbn == null) {
+            return
+        }
+
+        val packageName = sbn.packageName
+        if (TextUtils.isEmpty(packageName)
+            || packageName != AppDetectionService.GOOGLE_MAPS_PACKAGE
+            || sbn.notification.priority != Notification.PRIORITY_MAX
+            || !PrefUtils.isGmapsOnlyInNavigation(this)
+        ) {
+            return
+        }
+
+        AppDetectionService.get()?.setGmapsNavigating(false)
+
+        val intent = Intent(this, LimitService::class.java)
+        intent.putExtra(LimitService.EXTRA_NOTIF_CLOSE, true)
+        startService(intent)
+    }
+
+}
